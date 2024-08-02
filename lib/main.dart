59f4c0b01a968a8fff19759346d29deae120e84b
Mayank Goel <goyalmayank8700@gmail.com>
2024-08-02 22:23:13 +0530

    check

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:mojo_pizza_app_mvp/modules/adaptive_layout/adaptive_layout.dart';
-import 'package:mojo_pizza_app_mvp/modules/adaptive_layout/desktop_layout.dart';
-import 'package:mojo_pizza_app_mvp/modules/adaptive_layout/mobile_layout.dart';
-import 'package:mojo_pizza_app_mvp/modules/adaptive_layout/tab_layout.dart';
-import 'package:mojo_pizza_app_mvp/modules/splash/views/pages/splash_screen.dart';
+
+import 'modules/splash/views/pages/splash_screen.dart';
+
+// import 'package:mojo_pizza_app_mvp/modules/adaptive_layout/adaptive_layout.dart';
+// import 'package:mojo_pizza_app_mvp/modules/adaptive_layout/desktop_layout.dart';
+// import 'package:mojo_pizza_app_mvp/modules/adaptive_layout/mobile_layout.dart';
+// import 'package:mojo_pizza_app_mvp/modules/adaptive_layout/tab_layout.dart';
+// import 'package:mojo_pizza_app_mvp/modules/splash/views/pages/splash_screen.dart';
 
 /* void main() {
   runApp(const MaterialApp(
