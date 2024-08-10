e3b9bed5578ef09cdfd9c84518b219edc9f0227d
Pulkit Goyal <pulkitcgoyal@gmail.com>
2024-08-11 01:30:59 +0530

    add equatable dependency

--- lib/modules/cart/bloc/cart_state.dart
+++ lib/modules/cart/bloc/cart_state.dart
@@ -1,24 +1,24 @@
-import 'package:equatable/equatable.dart';
+// import 'package:equatable/equatable.dart';
 
-class CartItem {
-  final String id;
-  final String name;
-  final double price;
-  final int quantity;
+// class CartItem {
+//   final String id;
+//   final String name;
+//   final double price;
+//   final int quantity;
 
-  CartItem(
-      {required this.id,
-      required this.name,
-      required this.price,
-      required this.quantity});
-}
+//   CartItem(
+//       {required this.id,
+//       required this.name,
+//       required this.price,
+//       required this.quantity});
+// }
 
-class CartState extends Equatable {
-  final List<CartItem> cartItems;
-  final double totalAmount;
+// class CartState extends Equatable {
+//   final List<CartItem> cartItems;
+//   final double totalAmount;
 
-  const CartState({required this.cartItems, required this.totalAmount});
+//   const CartState({required this.cartItems, required this.totalAmount});
 
-  @override
-  List<Object> get props => [cartItems, totalAmount];
-}
+//   @override
+//   List<Object> get props => [cartItems, totalAmount];
+// }
